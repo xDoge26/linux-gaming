075c046be0336ee9e4a02082e3a89f6b3ea7e03a
Jank9525 <92904701+xDoge26@users.noreply.github.com>
2023-08-11 19:55:44 +0700

    Create debloat_mint.sh

---
+++ debloat_mint.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# Debloat mint
+sudo apt remove libreoffice* openjdk* thunderbird* mythes* papirus* yaru* numix* thingy xfce4-dict celluloid hypnotix pix rhythmbox webapp-manager hexchat transmission-gtk warpinator
+sudo apt autoremove
